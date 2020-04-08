7c32213a66d0840853b81aaf68cd4762e07bfc8f
Vitor Soares <vitor.r.soares@gmail.com>
2020-04-08 00:55:57 -0300

    finalizando aula 16

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {View} from 'react-native';
+import api from '../../services/api';
 
-export default function User() {
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: Prototypes.func
+    }).isRequired
+  }
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`)
+    this.setState({ stars: response.data});
+  }
+
+  render() {
+    const { starts } = this.state;
+
+    return <View />;
+  }
 }
